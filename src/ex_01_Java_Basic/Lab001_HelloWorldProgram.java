cc0473e16c7e5e3047ae66e0305a9a9044ba4113
TSPL-Code <shruthij9495@gmail.com>
2025-06-13 17:30:38 +0530

    13June2025

---
+++ src/ex_01_Java_Basic/Lab001_HelloWorldProgram.java
@@ -0,0 +1,10 @@
+package ex_01_Java_Basic;
+
+public class Lab001_HelloWorldProgram {
+    public static void main(String[] args) {
+
+       System.out.println("Hello World");
+       System.out.println("Thank you");
+    }
+
+}
