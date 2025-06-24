cdbdc3821fc2391b40adbc5c5e2d3deb553c2d06
TSPL-Code <shruthij9495@gmail.com>
2025-06-24 23:06:11 +0530

    StringBulider/StringBuffer

---
+++ src/ex_15_StringBuffer_Builder_StringFunctions/Lab146_StringBulider.java
@@ -0,0 +1,9 @@
+package ex_15_StringBuffer_Builder_StringFunctions;
+
+public class Lab146_StringBulider {
+    public static void main(String[] args) {
+        StringBuilder sb=new StringBuilder("Shruthi");
+        sb.append(123);
+        System.out.println(sb);
+    }
+}
