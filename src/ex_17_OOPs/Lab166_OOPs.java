8a8e2feea317d6db8262d56fdf00fccb58a60c1f
TSPL-Code <shruthij9495@gmail.com>
2025-07-01 17:50:39 +0530

    OOPs_Concepts

---
+++ src/ex_17_OOPs/Lab166_OOPs.java
@@ -0,0 +1,14 @@
+package ex_17_OOPs;
+
+public class Lab166_OOPs {
+    public static void main(String[] args) {
+
+        Person1 amit = new Person1();
+        Person1 pramod = new Person1();
+    }
+}
+class Person
+{
+
+}
+
